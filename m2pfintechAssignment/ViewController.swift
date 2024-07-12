59a956b4ab1c74f0687357800c8ca194bb597d0d
Susmita Pal <palsusmita68@gmail.com>
2024-07-12 16:32:14 +0530

    Initial Commit

---
+++ m2pfintechAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m2pfintechAssignment
+//
+//  Created by susmita on 12/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
